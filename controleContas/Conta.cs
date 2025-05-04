480accdc446e9d4b54d450e4c9b58b80e43c47e0
Breno Carvalho <103794884+BrenoC4rvalho@users.noreply.github.com>
2025-05-03 21:28:38 -0300

    Add project files.

---
+++ controleContas/Conta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controleContas
+{
+    class Conta
+    {
+        private int numero;
+        private decimal saldo; 
+
+        public Conta(int numero)
+        {
+            this.numero = numero;
+        }
+
+        public int NumeroProp {
+            get => numero;
+            set => numero = value;
+        }
+
+        public decimal SaldoProp
+        {
+            get
+            {
+                return saldo;
+            }
+            set
+            {
+                if (value >= 0.0m)
+                {
+                    saldo = value;
+                } else
+                {
+                    Console.WriteLine("O saldo não pode ser definido como negativo");
+                }
+            }
+        }
+
+        public void Deposito(decimal valor)
+        {
+            if(valor > 0.0m)
+            {
+                saldo += valor;
+            }
+        }
+    }
+}
---
+++ controleContas/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace controleContas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta1 = new Conta(123456);
+            conta1.SaldoProp = 1235.42m;
+            conta1.SaldoProp = -100m;
+            Console.Read();
+            Conta conta2 = new Conta(654321);
+            conta2.SaldoProp = 2341.43m;
+
+            Console.WriteLine("A conta de numero {0} tem saldo {1:C2}", conta1.NumeroProp, conta1.SaldoProp);
+            Console.WriteLine("A conta de numero {0} tem saldo {1:C2}", conta2.NumeroProp, conta2.SaldoProp);
+
+        }
+    }
+}

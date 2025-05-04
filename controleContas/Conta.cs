7063a4673986237620f630ebb4aed8822db0b444
Breno Carvalho <103794884+BrenoC4rvalho@users.noreply.github.com>
2025-05-03 21:56:49 -0300

    feat: transferencia e saque

--- controleContas/Conta.cs
+++ controleContas/Conta.cs
@@ -9,11 +9,23 @@ namespace controleContas
     class Conta
     {
         private int numero;
-        private decimal saldo; 
+        private decimal saldo;
+        private Cliente titular;
 
-        public Conta(int numero)
+        public Conta(int numero, decimal saldo, Cliente titular)
         {
             this.numero = numero;
+
+            if (saldo > 10.00m)
+            {
+                this.saldo = saldo;
+            }
+            else
+            {
+                throw new Exception("Saldo inicial deve ser maior que 10.00");
+            }
+
+            this.titular = titular;
         }
 
         public int NumeroProp {
@@ -44,6 +56,34 @@ namespace controleContas
             if(valor > 0.0m)
             {
                 saldo += valor;
+            }
+        }
+
+        public void Saque(decimal valor)
+        {
+            if (valor > 0.0m && (valor + 0.10m) < saldo )
+            {
+                saldo -= valor;
+                saldo -= 0.10m; // taxa de saque
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido para saque");
+            }
+        }
+
+        public void transferencia(Conta contaDestino, decimal valor)
+        {
+            if (valor > 0.0m && (valor) < saldo)
+            {
+                saldo -= valor;
+                contaDestino.Deposito(valor);
+
+                // exibir saldo das duas contas
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido para transferência");
             }
         }
     }
